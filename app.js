977b984650559e204207af13b02a1302b8a6c5e0
thepraveensingh <2021kuec2066@iiitkota.ac.in>
2024-10-12 18:07:17 +0530

    profile page created and posts also

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //middleware for protected route
 const isLoggedIn = (req,res,next) =>{
   if(req.cookies.token == ''){
-    res.send('you must be logged in')
+    res.redirect('/login')
   }
   else{
     let data = jwt.verify(req.cookies.token,'secret');
@@ -27,13 +27,29 @@ const isLoggedIn = (req,res,next) =>{
 app.get('/',(req,res)=>{
   res.render('index');
 });
+
 app.get('/login',(req,res)=>{
   res.render('login');
 });
-app.get('/profile',isLoggedIn,(req,res)=>{
-  console.log(req.user);
-  res.render('login')
+
+app.get('/profile',isLoggedIn,async(req,res)=>{
+  let user = await userModel.findOne({email: req.user.email}).populate('posts');
+  res.render('profile',{user})
 })
+
+app.post('/post',isLoggedIn,async(req,res)=>{
+  let user = await userModel.findOne({email: req.user.email})
+  let {content} = req.body;
+  let post = await postModel.create({
+    user : user._id,
+    content : content
+  })
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/profile')
+})
+
 app.post('/register',async(req,res)=>{
   let {email,password,username,name,age}= req.body;
   let user = await userModel.findOne({email});
@@ -41,7 +57,7 @@ app.post('/register',async(req,res)=>{
   bcrypt.genSalt(10,(err,salt) =>{
     bcrypt.hash(password,salt,async(err,hash)=>{
       let user = await userModel.create({
-        username,
+        username, 
         email,
         age,
         name,
@@ -64,16 +80,16 @@ app.post('/login',async(req,res)=>{
     if(result){
       let token = jwt.sign({email:email , userid : user._id},'secret')
       res.cookie('token',token);
-      res.status(200).send("u can login");
+      res.status(200).redirect('/profile');
  
     }
-    else res.redirect('/login');
+    else res.render('/login');
   })
 })
 
 app.get('/logout',(req,res)=>{
   res.cookie('token','');
-  res.redirect('/');
+  res.redirect('/login');
 })
 
 app.listen(3000);
